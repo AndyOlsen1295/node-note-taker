ab98a54ffb3bc5e33ee303b980fed3e0b95ab064
AndyOlsen1295 <112365233+AndyOlsen1295@users.noreply.github.com>
2023-04-16 15:27:24 -0600

    re-initialized package.json, then modified server.js file

--- server.js
+++ server.js
@@ -5,27 +5,29 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const allNotes = require('./db/db.json');
+const allNotes = require('./develop/db/db.json');
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('develop/public'));
 
 app.get('/api/notes', (req, res) => {
     res.json(allNotes.slice(1));
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, './develop/public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, './develop/public/notes.html'));
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, './develop/public/index.html'));
 });
+
 
 function createNewNote(body, notesArray) {
     const newNote = body;
